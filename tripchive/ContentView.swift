36a1dac661cf05cb4571ffb95be8ffb86cabeb18
Tom-the-Bomb <thello522@gmail.com>
2026-03-18 15:38:57 +0800

    Initial Commit

---
+++ tripchive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tripchive
+//
+//  Created by Tom the Bomb on 2026-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tripchive/tripchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripchiveApp.swift
+//  tripchive
+//
+//  Created by Tom the Bomb on 2026-03-18.
+//
+
+import SwiftUI
+
+@main
+struct tripchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
